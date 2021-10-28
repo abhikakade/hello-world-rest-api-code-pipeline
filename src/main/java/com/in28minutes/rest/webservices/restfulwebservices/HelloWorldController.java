5190a60bc2969a12ab5bc01c4f936cdbfd3bfbea
Abhikakade <abhikakade@gmail.com>
2021-10-28 17:48:27 +0530

    Update HelloWorldController.java

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World abhi!!";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
